8581b71d92d22d69cc8809f1a9adf1992b626756
OvershotHavik <plavetzky@gmail.com>
2021-12-06 11:55:03 -0500

    Initial Commit

---
+++ CoreDataDemo/CoreDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataDemoApp.swift
+//  CoreDataDemo
+//
+//  Created by Steve Plavetzky on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
